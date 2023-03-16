09ef9e011e128db848288d3dc9f179087bc6c8ba
PurushothamaG96 <purushothamachar96@gmail.com>
2023-03-16 10:51:02 +0530

    nav bars icon added

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from "react-router-dom"
 import "./Nav.css"
 function Nav() {
+    const [bar, setBar] = useState("fa-bars")
+    const [hideNav, setHideNav] = useState("close")
+    const handleBars =()=>{
+        if(bar==="fa-bars"){
+            setBar("fa-close")
+            setHideNav("open")
+        }
+        else{
+            setBar("fa-bars")
+            setHideNav("close")
+        }
+    }
     return (
         <nav>
             <div className='container nav-container'>
                 <h1>{`<`}<span className='text-primary'>Purushothama</span>{`/>`}</h1>
-                <ul>
+                <ul className={`${hideNav}`}>
                     <li className='px-3'><Link to={"/"} className="Link">Home</Link></li>
                     <li className='px-3'><Link to={"/project"} className="Link">Project</Link></li>
                     <li className='px-3'><Link to={"/education"} className="Link">Education</Link></li>
                     <li className='px-3'><Link to={"/contact"} className="Link">Contact me</Link></li>
                 </ul>
+                <button className='btn btn-white' id='hide-bars' onClick={handleBars}><i className={`fa ${bar} text-primary`}></i></button>
+                
+                
             </div>
         </nav>
 
